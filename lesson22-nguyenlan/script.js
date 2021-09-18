7836ddbcfb0739a74db201337a8d0f4a425d1078
nglan <nglan112000@gmail.com>
2021-09-19 02:21:01 +0700

    lesson22-nguyenlan submit code 03

--- lesson22-nguyenlan/script.js
+++ lesson22-nguyenlan/script.js
@@ -13,143 +13,127 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 var date = new Date()
 document.getElementById('month').textContent = monthNames[date.getMonth()] + " " + date.getFullYear();
 
+var incomeItems = [
+    {
+        description: "Salary",
+        value: 21000
+    },
+    {
+        description: "Sold car",
+        value: 15000
+    }
+];
+var expensesItems = [
+    {
+        description: "Apartment rent",
+        value: -900
+    },
+    {
+        description: "Grocery shopping",
+        value: -435
+    }
+];
 
+function deleteIncomeItem(index) {
+    incomeItems.splice(index, 1);
+    renderUI();
+};
 
-function addIncomItem(description, value, id) {
-    
-    return `
-        <div class="item clearfix" id="${id}">
-            <div class="item__description">${description}</div>
-            <div class="right clearfix">
-                <div class="item__value">+ ${value}</div>
-                <div class="item__delete">
-                    <button class="item__delete--btn" onclick="deleteIncomeItem('${id}')">
-                        <i class="ion-ios-close-outline"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    `
-}
+function deleteExpensesItem(index) {
+    expensesItems.splice(index, 1);
+    renderUI();
+};
 
-function addExpensesItem(description, value, id, percent) {
-    
-    return `
-        <div class="item clearfix" id="${id}">
-            <div class="item__description">${description}</div>
-            <div class="right clearfix">
-                <div class="item__value">- ${value}</div>
-                <div class="item__percentage">${percent}%</div>
-                <div class="item__delete">
-                    <button class="item__delete--btn"  onclick="updateExpeneseItem('${id}')">
-                        <i class="ion-ios-close-outline"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    `
-}
-
-function findIndex(arr, id){
-    for (let index = 0; index < arr.length; index++) {
-        if(arr[index].id === id){
-            return index;
-        }
+function renderIncome() {
+    let incomeList = "";
+    incomeItems.forEach((item, index) => {
+       
+        incomeList += `<div class="item clearfix">
+                    <div class="item__description">${item.description}</div>
+                    <div class="right clearfix">
+                        <div class="item__value">+ ${item.value}</div>
+                        <div class="item__delete">
+                            <button class="item__delete--btn" onclick="deleteIncomeItem(${index})">
+                                    <i class="ion-ios-close-outline"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>`
         
-    }
-}
-
-function deleteIncomeItem(id) {
-    elements = ""
-    incomes.splice(findIndex(incomes,id),1)
-    incomeFinal = 0
-    
-    incomes.forEach(element => {
-        incomeFinal += parseFloat(element.value)
-        elements += addIncomItem(element.description,
-                                element.value,
-                                element.id)
     });
-    document.getElementById("$('#incomeList')").innerHTML = elements
-    $('#incomeValue').text(`+ ${incomeFinal}`)
-    updateExpeneseItem(null)
-    
-}
 
-function updateExpeneseItem(id) {
-    elements = ""
-    if(id!=null){
-        expenses.splice(findIndex(expenses,id),1)
-    }
-    expensesFinal = 0
-    
-    for (let index = 0; index < expenses.length; index++) {
-        
-    }
-    expenses.forEach(element => {
-        expensesFinal += parseFloat(element.value)
-        percent = ((parseFloat(element.value)/incomeFinal)*100).toFixed(1)
-        if(percent==Number.POSITIVE_INFINITY || isNaN(percent)){
-            percent = "..."
-        }
-        elements += addExpensesItem(element.description,
-                                element.value,
-                                element.id,
-                                percent)
+    $('#incomeList').html(incomeList);
+};
+
+function percentExpenses(value) {
+    let sumIncome = incomeItems.reduce((currentValue, task) => currentValue + task.value, 0);
+    let percent = Math.abs(value) / sumIncome;
+    percent = Math.round(percent * 100);
+    return percent;
+};
+
+
+function renderExpense() {
+    let expensesList = "";
+    expensesItems.forEach((item, index) => {
+            expensesList += `<div class="item clearfix">
+                            <div class="item__description">${item.description}</div>
+                            <div class="right clearfix">
+                                <div class="item__value">${item.value}</div>
+                                <div class="item__percentage">${percentExpenses(item.value)}%</div>
+                                <div class="item__delete">
+                                    <button class="item__delete--btn" onclick="deleteExpensesItem(${index})"><i class="ion-ios-close-outline"></i></button>
+                                </div>
+                            </div>
+                        </div>`
     });
-    document.getElementById("$('#expensesList')").innerHTML = elements
-    $('#expensesValue').text(`- ${expensesFinal}`)
-    total = incomeFinal - expensesFinal
-    if($('#totalValue') >= 0){
-        $('#totalValue').text(`+ ${final}`)
-    }else{
-        $('#totalValue').text(`${final}`)
-    }
-    let percentFinal = (expensesFinal/incomeFinal * 100).toFixed(1)
-    if(percentFinal==Number.POSITIVE_INFINITY || isNaN(percentFinal)){
-        percentFinal = "..."
-    }
-    $('#expensesPercentage').text(`${percentFinal}%`)
-}
 
-$('#addButton').on("click", function() {
+    $('#expensesList').html(expensesList);
+};
 
-    if($('#selectAdd').val() === 'inc'){
-        let id = "incom-"+incomeCount
-        value = parseFloat($('#value').val()).toFixed(2)
-        income = {
-            description: $('#description').val(),
-            value: value,
-            id: id
-        }
-        incomes.push(income)
-        let element = addIncomItem($('#description').val(),
-                                    value,
-                                    id)
-        document.getElementById("incomeList").innerHTML += element
-        incomeFinal += parseFloat(parseFloat($('#value').val()).toFixed(2))
-        $('#incomeValue').text(`+ ${incomeFinal}`)
-        incomeCount++;
+function renderTotalValue() {
+    let sumIncome = incomeItems.reduce((currentValue, item) => currentValue + item.value, 0);
+    let sumExpense = expensesItems.reduce((currentValue, item) => currentValue + item.value, 0);
+    let totalValue = sumIncome - sumExpense;
+
+    $(".budget__income--value").html("+ " + sumIncome + ".00");
+    $(".budget__expenses--value").html( sumExpense + ".00");
+    if((totalValue) >= 0) {
+        $(".budget__value").html("+ " + totalValue + ".00");
     } else {
-        let id = "expenses-"+expensesCount
-        let value = parseFloat($('#value').val()).toFixed(2)
-        exp = {
-            description: $('#description').val(),
-            value: value,
-            id: id
-        }
-        expenses.push(exp)
-        percent = ((value/incomeFinal)*100).toFixed(1)
-        let element = addExpensesItem($('#description').val(),
-                                    value,
-                                    id,
-                                    percent)
-        document.getElementById("expensesList").innerHTML += element
-        expensesFinal += parseFloat(parseFloat($('#value').val()).toFixed(2))
-        $('#expensesValue').text(`- ${expensesFinal}`)
-        
-        expensesCount++;
+        $(".budget__value").html(totalValue + ".00");
     }
-    updateExpeneseItem(null)
 
-})
+    let percent = Math.abs(sumExpense) / sumIncome;
+    percent = Math.round(percent * 100);
+    $(".budget__expenses--percentage").html(percent + "%");
+    
+};
+
+function renderUI() {
+    renderExpense();
+    renderIncome();
+    renderTotalValue();
+}
+
+renderUI();
+
+$("#addButton").click(() => {
+    let item = {};
+    let description = $("#description").val();
+    let addType = $("#addType").val();
+    let addValue = $("#value").val();
+
+    item.description = description;
+    item.value = parseInt(addValue);
+
+    if (addType ==='inc') {
+        incomeItems.push(item);
+    } else {
+        expensesItems.push(item);
+    }
+    renderUI();
+    $("#description").val("");
+    $("#value").val("");
+});
+
